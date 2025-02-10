a339066e10929212c548ad5bbd4dc2a97f17909c
Yash Chandanshive <66194004+ycstudios@users.noreply.github.com>
2025-02-10 17:18:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-// Complete the js code
-function Car(make, model) {}
+class Car {
+    constructor(make, model) {
+        this.make = make;
+        this.model = model;
+    }
 
-function SportsCar(make, model, topSpeed) {}
+    getMakeModel() {
+        return `${this.make} ${this.model}`;
+    }
+}
+
+// SportsCar Class (inherits from Car)
+class SportsCar extends Car {
+    constructor(make, model, topSpeed) {
+        super(make, model); // Inheriting properties from Car
+        this.topSpeed = topSpeed;
+    }
+
+    getTopSpeed() {
+        return this.topSpeed;
+    }
+}
 
 // Do not change the code below
 window.Car = Car;
